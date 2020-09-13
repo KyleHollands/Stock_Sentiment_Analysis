93653fb58da3eee26ade06c3da727faff817d6cc
Kyle Hollands <kylehollands@gmail.com>
2020-09-13 15:44:09 -0400

    REASON: Script reads the numer of Bears and Bulls that people have deemed the stock to be. Implemented a basic scroll function to load more posts beyond the initial page.
    Need to extract time information to be used for a specific time period,
    such as the last 8 hours.

--- stock_sentiment_analysis.py
+++ stock_sentiment_analysis.py
@@ -18,19 +18,51 @@ def parse(PATH, driver, website):
     # search = driver.find_element_by_class_name("infinite-scroll-component ")
     # print(driver.page_source)
 
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
     try:
         scroller = WebDriverWait(driver, 2).until(
             EC.presence_of_element_located((By.CLASS_NAME, "infinite-scroll-component "))
         )
-        print(scroller.text)
-    except:
+        # print(scroller.text)
+
+        bull_count = 0
+        bear_count = 0     
+
+        for i in range(1, 30):
+            try:
+                # stock_comments = scroller.find_element_by_xpath("/html/body/div[2]/div/div/div[3]/div/div/div/div[2]/div/div/div[2]/div[3]/div/div[%i]/div/div/article" %(i))
+                stock_outlook = scroller.find_element_by_xpath("/html/body/div[2]/div/div/div[3]/div/div/div/div[2]/div/div/div[2]/div[3]/div/div[%i]/div/div/article/div/div[2]/div[1]/span[2]/span/div/div" %(i))
+                # print(stock_comments.text)
+        
+                if stock_outlook.text == "Bullish":
+                    bull_count += 1
+                elif stock_outlook.text == "Bearish":
+                    bear_count += 1
+                else:
+                    pass
+
+                # print(stock_outlook.text)
+            except:
+                pass
+        
+        print("Bulls: " + str(bull_count))
+        print("Bears: " + str(bear_count))
+
+        # for article in articles:
+        #     # header = article.find_element_by_class_name("st_11GoBZI")
+        #     header = article.find_element_by_class_name("st_11GoBZI")
+
+            # print(header.text)
+            
+    finally:
         driver.quit()
 
     # scroller = driver.find_element_by_class_name("infinite-scroll-component ")
     # print(scroller.text)
     # time.sleep(5)
 
-    driver.quit()
+    # driver.quit()
 
 def main(argv):
     # Path to the driver. In this case, Chrome.
