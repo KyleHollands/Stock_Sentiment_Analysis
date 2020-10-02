3e02ed596b4e0d2cf3ba41663eab590e4e623a23
Kyle Hollands <kylehollands@gmail.com>
2020-10-02 17:19:59 -0400

    REASON: Added an example image of the script.

--- stock_sentiment_analysis.py
+++ stock_sentiment_analysis.py
@@ -25,8 +25,8 @@ def parse(PATH, driver, website, time_pattern, bullish_pattern, bearish_pattern,
         scroller = WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.CLASS_NAME, "infinite-scroll-component "))
         )
-        # print(scroller.text)
-
+        # print(scroller.text) 
+    
         # Sets the bull and bear counters to 0.
         bull_count = 0
         bear_count = 0 
@@ -41,7 +41,7 @@ def parse(PATH, driver, website, time_pattern, bullish_pattern, bearish_pattern,
 
         while True:
             # Scroll down to the preset pixel length of the page.
-            driver.execute_script("window.scrollTo(0, 100000)") # Set to a high value.
+            driver.execute_script("window.scrollTo(0, 500000)") # Set to a high value.
 
             # Utilize the pause value above.
             time.sleep(SCROLL_PAUSE_TIME)
@@ -64,47 +64,50 @@ def parse(PATH, driver, website, time_pattern, bullish_pattern, bearish_pattern,
                 else:
                     pass
 
-                # Create a counter of how many times Bullish or Bearish appear.
+                # Create a counter of how many times Bullish or Bearish appear and whether positive or negative comments are found.
                 if re.search(bullish_pattern, stock_comment.text):
                     bull_count += 1
                 elif re.search(bearish_pattern, stock_comment.text):
                     bear_count += 1
                 else:
-                    pass
-
-                if any(x in stock_comment.text.lower() for x in positive_list):
-                    # print("Found Positive")
-                    positive_count += 1
-                
-                if any(x in stock_comment.text.lower() for x in negative_list):
-                    # print("Found Negative")
-                    negative_count += 1
-
-                # result = [ele for ele in positive_list if (ele in stock_comment.text.lower())]
-                # print(bool(result))
-
+                    if any(x in stock_comment.text.lower() for x in positive_list):
+                        # print("Found Positive")
+                        positive_count += 1
+                    elif any(x in stock_comment.text.lower() for x in negative_list):
+                        # print("Found Negative")
+                        negative_count += 1
+                    else:
+                        pass
             except:
                 pass
             
         # Print out bearish/bullish results as well as a calculated percentage reflecting overall sentiment========================
 
         # Print out the results while explicitly converting the variables to strings.
-        print("Bulls: " + str(bull_count))
+        print("\nBulls: " + str(bull_count))
         print("Bears: " + str(bear_count))
-        print(positive_count)
-        print(negative_count)
-
+        
+        print("\nPositive comments: " + str(positive_count))
+        print("Negative comments: " + str(negative_count))
+        
         positive_total = (positive_count + bull_count)
         negative_total = (negative_count + bear_count)
-        
 
+        print("\nBulls + positive comments: " + str(positive_total))
+        print("Bears + negative comments: " + str(negative_total))
+        
         # Determine positive outlook percentage.
         if negative_total < positive_total:
             percentage = 100 - ((negative_total / positive_total) * 100)
-            print("Stock Sentiment: " + "{:.2f}".format(percentage) + "%")
+            print("\nStock Sentiment: " + "{:.2f}".format(percentage) + "%")
+        elif negative_total > positive_total:
+            percentage = 100 - ((positive_total / negative_total) * 100)
+            print("\nStock Sentiment: " + "{:.2f}".format(percentage) + "%")
+        elif negative_total and positive_total == 0:
+            print("\nNo data found.")
         else:
-            percentage = ((positive_total / negative_total) * 100)
-            print("Stock Sentiment: " + "{:.2f}".format(percentage) + "%")
+            percentage = ((positive_total / negative_total) * 50)
+            print("\nStock Sentiment: " + "{:.2f}".format(percentage) + "%")
 
         # Wait for user input to prevent window from closing.
         input('\nPress ENTER to exit')
@@ -135,15 +138,15 @@ def main(argv):
     # Acquire time to scroll to from the user. (11:00 AM). Only considers hour and AM/PM.
     time = input("\nEnter the time to search until (ex. 03:00 PM): ")
     
-    # Time search regex pattern.
+    # Regex search patterns.
     time_pattern = r''+time[0:2]+':\d\d\s'+time[6:8]+''
     bullish_pattern = r'Bullish'
     bearish_pattern = r'Bearish'
 
     positive_count = 0
     negative_count = 0
-    positive_list = ["buy", "up", "increase", "better", "great", "healthy", "consolidation"]
-    negative_list = ["sell", "overvalued", "shrinking"]
+    positive_list = ["buy", "up", "increase", "better", "great", "healthy", "consolidation", "undervalued", "the squeeze", "outperform", "outperforms"]
+    negative_list = ["sell", "overvalued", "shrinking", "down", "underperform", "underperforms"]
 
     # Calls the main functioon above and passes the variables set previously.
     parser = parse(PATH, driver, website, time_pattern, bullish_pattern, bearish_pattern, positive_count, negative_count, positive_list, negative_list)
