6e865544760c0bf75671bb8190b95da2da15305b
Kyle Hollands <kylehollands@gmail.com>
2020-10-21 18:05:14 -0400

    REASON: Adjusted how the percentage is calculated and cleaned up some other various parts of the code. * Removed a couple try blocks for testing.

--- stock_sentiment_analysis.py
+++ stock_sentiment_analysis.py
@@ -3,7 +3,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import string
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -29,105 +28,107 @@ def parse(driver, website, date_time, bullish_pattern, bearish_pattern, positive
         date_time_pattern = r''+date_time+''
 
     # Using a try, except block, ensure the content is loaded before continuing by waiting.
-    try:
-        scroller = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "infinite-scroll-component "))
-        )
-        # print(scroller.text) 
+    # try:
+    scroller = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "infinite-scroll-component "))
+    )
+    # print(scroller.text) 
+
+    # Sets the bull and bear counters to 0.
+    bull_count = 0
+    bear_count = 0 
     
-        # Sets the bull and bear counters to 0.
-        bull_count = 0
-        bear_count = 0 
+    # Controls the page scrolling parameters==================================================================================
+    
+    # Control the pause time before scrolling begins to allow page loading.
+    SCROLL_PAUSE_TIME = 0.5
+
+    # Acquire the page scroll height.
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to the preset pixel length of the page.
+        driver.execute_script("window.scrollTo(0, 50000)") # Set to a high value.
+
+        # Utilize the pause value above.
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height.
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    # The main loop to acquire header and comment information================================================================
+    for stock_comment in scroller.find_elements_by_css_selector('.st_24ON8Bp.st_1x3QBA7.st_1SZeGna.st_3MXcQ5h.st_3-tdfjd'):
+        stock_comment_text = stock_comment.text.lower()
+        stock_comment_text = (stock_comment_text.translate({ord(i): None for i in "'\''-?!"}))
         
-        # Controls the page scrolling parameters==================================================================================
-        
-        # Control the pause time before scrolling begins to allow page loading.
-        SCROLL_PAUSE_TIME = 0.5
+        # try:
+        print("-----------------------------------------------------------------")
+        print(stock_comment_text) # For debugging.
+        print("-----------------------------------------------------------------")
 
-        # Acquire the page scroll height.
-        last_height = driver.execute_script("return document.body.scrollHeight")
-
-        while True:
-            # Scroll down to the preset pixel length of the page.
-            driver.execute_script("window.scrollTo(0, 500000)") # Set to a high value.
-
-            # Utilize the pause value above.
-            time.sleep(SCROLL_PAUSE_TIME)
-
-            # Calculate new scroll height and compare with last scroll height.
-            new_height = driver.execute_script("return document.body.scrollHeight")
-
-            if new_height == last_height:
-                break
-            last_height = new_height
-
-        # The main loop to acquire header and comment information================================================================
-        for stock_comment in scroller.find_elements_by_css_selector('.st_24ON8Bp.st_1x3QBA7.st_1SZeGna.st_3MXcQ5h.st_3-tdfjd'):
-            # stock_comment_text = stock_comment.text.translate(str.maketrans('', '', string.punctuation))
-            stock_comment_text = stock_comment.text.lower()
-            # stock_comment_text = stock_comment_text.replace("'", "")
-            stock_comment_text = (stock_comment_text.translate({ord(i): None for i in "'\''-?!"}))
-            # stock_comment_text = stock_comment_text.replace("'", "").replace("!", "").replace("?", "")
-            try:
-                print(stock_comment_text) # For debugging.
-
-                if re.search(date_time_pattern, stock_comment_text):
-                    break
-                else:
-                    pass
-
-                # Create a counter of how many times Bullish or Bearish appear and whether positive or negative comments are found.
-                if re.search(bullish_pattern, stock_comment_text):
-                    bull_count += 1
-                elif re.search(bearish_pattern, stock_comment_text):
-                    bear_count += 1
-                else:
-                    if any(x in stock_comment_text for x in positive_list):
-                        # print("Found Positive")
-                        positive_count += 1
-                    elif any(x in stock_comment_text for x in negative_list):
-                        # print("Found Negative")
-                        negative_count += 1
-                    else:
-                        pass
-            except:
-                pass
-            
-        # Print out bearish/bullish results as well as a calculated percentage reflecting overall sentiment========================
-
-        # Print out the results while explicitly converting the variables to strings.
-        print("\nBulls: " + str(bull_count))
-        print("Bears: " + str(bear_count))
-        
-        print("\nPositive comments: " + str(positive_count))
-        print("Negative comments: " + str(negative_count))
-        
-        positive_total = (positive_count + bull_count)
-        negative_total = (negative_count + bear_count)
-
-        print("\nBulls + positive comments: " + str(positive_total))
-        print("Bears + negative comments: " + str(negative_total))
-        
-        # Determine positive outlook percentage.
-        if negative_total < positive_total:
-            percentage = 100 - ((negative_total / positive_total) * 100)
-            print("\nStock Sentiment: " + "{:.2f}".format(percentage) + "%")
-        elif negative_total > positive_total:
-            percentage = 100 - ((positive_total / negative_total) * 100)
-            print("\nStock Sentiment: " + "{:.2f}".format(percentage) + "%")
-        elif negative_total and positive_total == 0:
-            print("\nNo data found.")
+        if re.search(date_time_pattern, stock_comment_text):
+            break
         else:
-            percentage = ((positive_total / negative_total) * 50)
-            print("\nStock Sentiment: " + "{:.2f}".format(percentage) + "%")
+            pass
 
-        # Wait for user input to prevent window from closing.
-        input('\nPress ENTER to exit')
+        # Create a counter of how many times Bullish or Bearish appear and whether positive or negative comments are found.
+        if re.search(bullish_pattern, stock_comment_text):
+            bull_count += 1
+        elif re.search(bearish_pattern, stock_comment_text):
+            bear_count += 1
+        else:
+            if any(x in stock_comment_text for x in positive_list):
+                # print("Found Positive")
+                positive_count += 1
+            elif any(x in stock_comment_text for x in negative_list):
+                # print("Found Negative")
+                negative_count += 1
+            else:
+                pass
+        # except:
+        #     pass
+        
+    # Print out bearish/bullish results as well as a calculated percentage reflecting overall sentiment========================
 
-        # ==========================================================================================================================
+    # Print out the results while explicitly converting the variables to strings.
+    # print("\nBulls: " + str(bull_count))
+    # print("Bears: " + str(bear_count))
+    
+    # print("\nPositive comments: " + str(positive_count))
+    # print("Negative comments: " + str(negative_count))
+    
+    positive_total = (positive_count + bull_count)
+    negative_total = (negative_count + bear_count)
 
-    finally:
-        driver.quit()
+    print("\nBulls + positive comments: " + str(positive_total))
+    print("Bears + negative comments: " + str(negative_total))
+    
+    # Determine positive outlook percentage.
+    if positive_total > negative_total:
+        percentage = 100 - ((negative_total / positive_total) * 100)
+        print("\nStock Sentiment: " + "{:.2f}".format(percentage) + "%")
+    elif negative_total > positive_total:
+        percentage = ((positive_total / negative_total) * 100)
+        print("\nStock Sentiment: " + "{:.2f}".format(percentage) + "%")
+    elif negative_total == 0 and positive_total == 0:
+        print("\nNo positive or negative data found.")
+    elif negative_total == positive_total:
+        percentage = ((positive_total / negative_total) * 50)
+        print("\nStock Sentiment: " + "{:.2f}".format(percentage) + "%")
+    else:
+        pass
+
+    # Wait for user input to prevent window from closing.
+    input('\nPress ENTER to exit')
+
+    # ==========================================================================================================================
+
+    # finally:
+    driver.quit()
 
 def main(argv):
     # Path to Chrome driver.
@@ -139,6 +140,11 @@ def main(argv):
     options.add_argument('--disable-gpu')
     options.add_argument('--log-level=3')
     options.add_argument('--lang=en')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.add_argument('--ignore-urlfetcher-cert-requests')
+    options.add_argument('--allow-insecure-localhost')
+    options.add_argument('--ignore-certificate-errors-spki-list')
 
     # Sets the driver to the Chrome webdriver utilizing the options set above.
     driver = webdriver.Chrome(executable_path=PATH, options=options)
@@ -148,7 +154,7 @@ def main(argv):
     website = "https://www.stocktwits.com/symbol/" + stock
 
     # Acquire time to scroll to from the user. (11:00 AM). Only considers hour and AM/PM.
-    print("\nEnter the date (ex. 10/3/20) or time (ex. 03:00 PM  | minutes excluded.)")
+    print("\nEnter the date (ex. 10/3/20) or time (ex. 03:00 PM  | minutes excluded from search.)")
     print("----------------")
     date_time = input()
 
@@ -158,10 +164,10 @@ def main(argv):
     positive_count = 0
     negative_count = 0
     
-    positive_list = ["buy", "up", "increase", "better", "great", "healthy", "consolidation", "undervalued", "the squeeze", "outperform", "outperforms", "thats a bullish close", "recover", "squeeze"]
-    negative_list = ["sell", "overvalued", "shrinking", "down", "underperform", "underperforms", "thats a bearish close", "cut my losses"]
+    positive_list = ["buy", "up", "increase", "better", "great", "healthy", "consolidation", "undervalued", "the squeeze", "outperform", "outperforms", "thats a bullish close", "recover", "squeeze", "short it again"]
+    negative_list = ["sell", "overvalued", "shrinking", "down", "underperform", "underperforms", "thats a bearish close", "cut my losses", "get out", "bye bye"]
 
-    # Calls the main functioon above and passes the variables set previously.
+    # Calls the main function above and passes the variables set previously.
     parser = parse(driver, website, date_time, bullish_pattern, bearish_pattern, positive_count, negative_count, positive_list, negative_list)
 
 if __name__ == "__main__":
